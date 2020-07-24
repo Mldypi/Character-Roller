8aff3bac64fb665e30e13d5f14782bd844cf9cbd
aaawheel <acwheel@hotmail.com>
2020-07-24 16:07:08 +0000

    Added prof_bonus to class PlayerCharacter

--- main.py
+++ main.py
@@ -28,13 +28,14 @@ intelligence = s_arr[3]
 wisdom = s_arr[4]
 charisma = s_arr[5]
 
-#Stat variables go here
+#Stat variables go here 
 str_score = d_arr[0]
 dex_score = d_arr[1]
 con_score = d_arr[2]
 int_score = d_arr[3]
 wis_score = d_arr[4]
 cha_score = d_arr[5]
+
 
 #print("unmodified str score: " +str(str_score))   #prints str_score for tests
 
@@ -53,6 +54,59 @@ con_mod = ((con_score//2)-5)
 int_mod = ((int_score//2)-5)
 wis_mod = ((wis_score//2)-5)
 cha_mod = ((cha_score//2)-5)
+
+#skills will go here
+Skills = {
+  'strength': ['athletics',],
+  'dexterity': ['acrobatics', 'sleight_of_hand', 'stealth'],
+  'intelligence': ['arcana', 'history', 'investigation', 'nature', 'religion'],
+  'wisdom': ['animal_handling', 'insight', 'medicine', 'perception', 'survival'],
+  'charisma': ['deception', 'intimidation', 'performance', 'persuasion']
+   }
+
+#creating a class for the player character as a whole. Need to call the following classes into this somehow later: character_class, race, attributes (not built yet), Skills into this.
+class PlayerCharacter:
+  def __init__(self, *attributes, **kw):
+    self.character_name = None
+    self.total_character_level = None
+    self.prof_bonus = None
+    self.attributes = None
+    self.attribute_bonuses = None
+    self.armor_class = None
+    self.initiative = None
+    self.total_hit_dice = None
+    self.max_hit_points = None
+    self.languages = None
+    self.background = None
+    self.ideas = None
+    self.bonds = None
+    self.flaws = None
+    self.traits = None
+  
+  def character_name(self):
+    character_name = input("What is your name?")
+    return (character_name)
+
+  def prof_bonus(self):
+    if total_character_level >= 1 and total_character_level <= 4:
+      self.prof_bonus = 2
+    if total_character_level >= 5 and total_character_level <= 8:
+      self.prof_bonus = 3
+    if total_character_level >= 9 and total_character_level <= 12:
+      self.prof_bonus = 4
+    if total_character_level >= 13 and total_character_level <= 16:
+      self.prof_bonus = 5
+    if total_character_level >= 17 and total_character_level <= 20:
+      self.prof_bonus = 6
+    return self.prof_bonus
+
+
+new_char = PlayerCharacter()
+new_char.prof_bonus()
+new_char.total_character_level = 5
+print (new_char.prof_bonus)
+
+
 
 #Races will go here. Class has kw arg so that these items cna be referenced in other functions. These items are the defaults, applied to all races - exceptions are written into the races (most races are medium, so medium is the default. halflings are small, so will have a 'size' of 'small'.)
 #Dragonborn, Dwarf, Elf, Gnome, Half-elf, Halfling, Half-Orc, Human, Tiefling
@@ -96,36 +150,30 @@ Races = {
 
 #adds racial attribute bonus to str_score and redefines str_score as the new number. Will edit this later to be either universal, or add more equations per race.
 
-def racial_attrib_score():
-  racial_attrib = (str_score + (Races['mountain_dwarf'].stat_mods['stre']))
-  return int(racial_attrib)
+#def racial_attrib_score():
+ # racial_attrib = (str_score + (Races['mountain_dwarf'].stat_mods['stre']))
+  #return int(racial_attrib)
 
-str_score = racial_attrib_score()
+#str_score = racial_attrib_score()
 
 #print("str with racial score: " + str(str_score))   #test print to see if attribute adder worked
 
-#skills will go here
-Skills = {
-  'strength': ['athletics',],
-  'dexterity': ['acrobatics', 'sleight_of_hand', 'stealth'],
-  'intelligence': ['arcana', 'history', 'investigation', 'nature', 'religion'],
-  'wisdom': ['animal_handling', 'insight', 'medicine', 'perception', 'survival'],
-  'charisma': ['deception', 'intimidation', 'performance', 'persuasion']
-   }
 
-#Contains features common to all classes
+
+#Contains features common to all classes. Proficiencies contains armor, weapons, tools, saving throws, skills
 class CharacterClass:
   def __init__(self, character_class, **kw):
     self.character_class = character_class
-    self.class_skills = kw.get('class_skills')
     self.class_features = None
     self.hit_dice = None
     self.proficiencies = None
+    self.class_level = 1
 
 
 
 #Fighter is an instance of the CharacterClass class, this will go for all classes unless I use a JSON file.
 Fighter = CharacterClass("fighter")
+
 Fighter.class_features = {
   'fight_lvl_1': ['fighting_style', 'second_wind'],
   'fight_lvl_2': ['action_surge_1'],
@@ -147,12 +195,10 @@ Fighter.class_features = {
   'fight_lvl_18': ['martial_archetype_feature_4'],
   'fight_lvl_19': ['asi_7'],
   'fight_lvl_20': ['extra_attack_3'],
-
 }
 
-Fighter.hit_dice = {
+Fighter.hit_dice = 10
 
-}
 
 Fighter.proficiencies = {
   'armor': ['light_armor', 'medium_armor', 'heavy_armor'],
@@ -161,5 +207,13 @@ Fighter.proficiencies = {
   'skills': ['acrobatics', 'animal_handling', 'athletics', 'history', 'insight', 'intimidation', 'perception', 'survival']
   }
 
-print(Fighter.class_features['fight_lvl_1'])
-print(Fighter.proficiencies['weapons'])
+#test of starting hp roll function for fighter
+#def d10_starting_hp():
+#  _ = Fighter.hit_dice + con_mod
+#  return(_)
+
+#d10_starting_hp = starting_hp()
+#print(starting_hp)
+
+#print(Fighter.class_features['fight_lvl_1'])
+#print(Fighter.proficiencies['weapons'])
